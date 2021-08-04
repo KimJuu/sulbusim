223f40c84989e7914a8266d7fbb737eea8754d79
Kimjuu <point8622@gmail.com>
2021-08-04 18:25:05 +0900

    Initial commit

---
+++ src/main/java/com/sulbusim/sulbusim/SulbusimApplication.java
@@ -0,0 +1,13 @@
+package com.sulbusim.sulbusim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulbusimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SulbusimApplication.class, args);
+	}
+
+}
